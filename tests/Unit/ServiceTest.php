c6f2eacaa4e15fdf83f930d7473b0c959279de57
Ashley Clarke <me@ashleyclarke.me>
2017-10-31 22:25:06 +0100

    add tests

--- tests/Unit/ServiceTest.php
+++ tests/Unit/ServiceTest.php
@@ -2,6 +2,7 @@
 
 namespace Shield\Box\Test\Unit;
 
+use Carbon\Carbon;
 use PHPUnit\Framework\Assert;
 use Shield\Shield\Contracts\Service;
 use Shield\Testing\TestCase;
@@ -33,15 +34,77 @@ class ServiceTest extends TestCase
     }
 
     /** @test */
-    public function it_can_verify_a_valid_request()
+    public function it_can_verify_a_valid_request_with_primary()
     {
+        $this->app['config']['shield.services.box.options.primary'] = 'primary';
+        $this->app['config']['shield.services.box.options.secondary'] = 'secondary';
 
+        $timestamp = Carbon::now();
+
+        $request = $this->request('anything');
+        $request->headers->add([
+            'BOX-DELIVERY-TIMESTAMP' => $timestamp->toAtomString(),
+            'BOX-SIGNATURE-PRIMARY' => base64_encode(hash_hmac('sha256', 'anything' . $timestamp->toAtomString(), 'primary', true)),
+            'BOX-SIGNATURE-SECONDARY' => base64_encode(hash_hmac('sha256', 'anythingx' . $timestamp->toAtomString(), 'secondary', true)),
+        ]);
+
+        Assert::assertTrue($this->service->verify($request, collect($this->app['config']['shield.services.box.options'])));
+    }
+
+    /** @test */
+    public function it_can_verify_a_valid_request_with_secondary()
+    {
+        $this->app['config']['shield.services.box.options.primary'] = 'primary';
+        $this->app['config']['shield.services.box.options.secondary'] = 'secondary';
+
+        $timestamp = Carbon::now();
+
+        $request = $this->request('anything');
+        $request->headers->add([
+            'BOX-DELIVERY-TIMESTAMP' => $timestamp->toAtomString(),
+            'BOX-SIGNATURE-PRIMARY' => base64_encode(hash_hmac('sha256', 'anythingx' . $timestamp->toAtomString(), 'primary', true)),
+            'BOX-SIGNATURE-SECONDARY' => base64_encode(hash_hmac('sha256', 'anything' . $timestamp->toAtomString(), 'secondary', true)),
+        ]);
+
+        Assert::assertTrue($this->service->verify($request, collect($this->app['config']['shield.services.box.options'])));
+    }
+
+    /**
+     * @test
+     */
+    public function it_will_not_verify_an_old_request()
+    {
+        $this->app['config']['shield.services.box.options.primary'] = 'primary';
+        $this->app['config']['shield.services.box.options.secondary'] = 'secondary';
+
+        $timestamp = Carbon::now()->subMinutes(11);
+
+        $request = $this->request('anything');
+        $request->headers->add([
+            'BOX-DELIVERY-TIMESTAMP' => $timestamp->toAtomString(),
+            'BOX-SIGNATURE-PRIMARY' => base64_encode(hash_hmac('sha256', 'anything' . $timestamp->toAtomString(), 'primary', true)),
+            'BOX-SIGNATURE-SECONDARY' => base64_encode(hash_hmac('sha256', 'anything' . $timestamp->toAtomString(), 'secondary', true)),
+        ]);
+
+        Assert::assertFalse($this->service->verify($request, collect($this->app['config']['shield.services.box.options'])));
     }
 
     /** @test */
     public function it_will_not_verify_a_bad_request()
     {
+        $this->app['config']['shield.services.box.options.primary'] = 'primary';
+        $this->app['config']['shield.services.box.options.secondary'] = 'secondary';
 
+        $timestamp = Carbon::now();
+
+        $request = $this->request('anything');
+        $request->headers->add([
+            'BOX-DELIVERY-TIMESTAMP' => $timestamp->toAtomString(),
+            'BOX-SIGNATURE-PRIMARY' => base64_encode(hash_hmac('sha256', 'anythingx' . $timestamp->toAtomString(), 'primary', true)),
+            'BOX-SIGNATURE-SECONDARY' => base64_encode(hash_hmac('sha256', 'anythingx' . $timestamp->toAtomString(), 'secondary', true)),
+        ]);
+
+        Assert::assertFalse($this->service->verify($request, collect($this->app['config']['shield.services.box.options'])));
     }
 
     /** @test */
